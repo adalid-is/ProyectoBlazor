48f340be119f9c60bd629cca143d1c557d828ceb
adalid-is <aadalid@integrasoftware.biz>
2024-11-28 16:43:24 -0300

    subir grid get completa

---
+++ Common/OE01/DTODSGeneral.cs
@@ -0,0 +1,28 @@
+﻿
+namespace BlazorOE01.Common.OE01
+
+
+{
+    public class DTODSGeneral
+    {
+        public Dsgeneral dsEmployee { get; set; }
+    }
+
+    public class Dsgeneral
+    {
+        public TTEmployee[] ttEmployee { get; set; }
+    }
+    public class TTEmployee
+    {
+        public int EmpNum { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
